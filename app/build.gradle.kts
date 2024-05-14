50993ab0c828facf446d663d7b73225a52ddc54e
wautischer <wautischer@outlook.com>
2024-05-14 12:58:16 +0200

    init fetch data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
 }
 
 android {
@@ -50,6 +51,11 @@ android {
 }
 
 dependencies {
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    implementation("io.coil-kt:coil-compose:2.4.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
--- app/src/main/java/at/wautschaar/raterightlight/MainActivity.kt
+++ app/src/main/java/at/wautschaar/raterightlight/MainActivity.kt
@@ -3,13 +3,23 @@ package at.wautschaar.raterightlight
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import at.wautschaar.raterightlight.model.Book
+import at.wautschaar.raterightlight.network.API
 import at.wautschaar.raterightlight.ui.theme.RateRightLightTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +27,51 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             RateRightLightTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    var bookListByName by remember {
+                        mutableStateOf<List<Book>>(emptyList())
+                    }
+                    LaunchedEffect(Unit) {
+                        val bookResponse = API.retrofitService.getBooks("Berserk")
+                        bookListByName = bookResponse.items.map { bookItem ->
+                            Book(
+                                id = bookItem.id,
+                                title = bookItem.volumeInfo.title,
+                                authors = bookItem.volumeInfo.authors,
+                                description = bookItem.volumeInfo.description,
+                                publishedDate = bookItem.volumeInfo.publishedDate,
+                                pageCount = bookItem.volumeInfo.pageCount,
+                                categories = bookItem.volumeInfo.category,
+                                language = bookItem.volumeInfo.language,
+                                imageUrl = bookItem.volumeInfo.imageLinks.toString()
+                            )
+                        }
+                    }
+                    BookList(books = bookListByName)
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BookCard (book: Book, modifier: Modifier = Modifier) {
+    Card(modifier = Modifier) {
+        Column {
+            Text(text = book.title)
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    RateRightLightTheme {
-        Greeting("Android")
+fun BookList (books: List<Book>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = Modifier) {
+        items(books) {tempBook ->
+            BookCard(book = tempBook, modifier = Modifier)
+        }
     }
 }
