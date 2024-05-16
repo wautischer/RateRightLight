fdd4dc12d510e3ba2576456a5d8f0cf355d6df7e
wautischer <wautischer@outlook.com>
2024-05-16 13:15:42 +0200

    debug errors from mergin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     implementation ("androidx.navigation:navigation-compose:2.7.7")
 
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
