dce031dac351e3183ef115a573000102ff6138b7
wautischer <wautischer@outlook.com>
2024-05-16 11:44:12 +0200

    test query parameter

--- app/src/main/java/at/wautschaar/raterightlight/MainActivity.kt
+++ app/src/main/java/at/wautschaar/raterightlight/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : ComponentActivity() {
                         mutableStateOf<List<Book>>(emptyList())
                     }
                     LaunchedEffect(Unit) {
-                        val bookResponse = API.retrofitService.getBooks("Berserk")
+                        val bookResponse = API.retrofitService.getBooks("berserk-max-")
                         bookListByName = bookResponse.items.map { bookItem ->
                             Book(
                                 id = bookItem.id,
